13f5dee6445a456a8adfb7a93552a8f45b185124
drozdov <drozdov@skytracking.ru>
2022-02-02 10:30:10 +0300

    add event-bus and block

---
+++ src/utils/block.ts
@@ -0,0 +1,142 @@
+import EventBus from './event-bus';
+
+type PropsType = Record<string, any>;
+
+export default class Block<P extends PropsType> {
+  static EVENTS = {
+    INIT: 'init',
+    FLOW_CDM: 'flow:component-did-mount',
+    FLOW_CDU: 'flow:component-did-update',
+    FLOW_RENDER: 'flow:render',
+  };
+
+  private _element: HTMLElement | null = null;
+  private _meta: any = null;
+
+  private eventBus: () => EventBus;
+  public props: P | object;
+
+  constructor(tagName = 'div', props: P) {
+    const eventBus = new EventBus();
+    this._meta = {
+      tagName,
+      props,
+    };
+
+    this.props = this._makePropsProxy(props);
+
+    this.eventBus = () => eventBus;
+
+    this._registerEvents(eventBus);
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  private _registerEvents(eventBus: EventBus) {
+    eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+  }
+
+  private _createResources() {
+    const { tagName } = this._meta;
+    this._element = this._createDocumentElement(tagName);
+  }
+
+  public init() {
+    this._createResources();
+    this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  private _componentDidMount() {
+    this.componentDidMount();
+  }
+
+  // Может переопределять пользователь, необязательно трогать
+  public componentDidMount() {
+    //componentDIdMount
+  }
+
+  // public dispatchComponentDidMoun() {
+  //   this._eventBus().emit(Block.EVENTS.FLOW_CDM);
+  // }
+
+  private _componentDidUpdate(oldProps: P, newProps: P) {
+    const response = this.componentDidUpdate(oldProps, newProps);
+    if (!response) {
+      return;
+    }
+    this._render();
+  }
+
+  // @ts-ignore
+  // Может переопределять пользователь, необязательно трогать
+  public componentDidUpdate(oldProps: P, newProps: P) {
+    return true;
+  }
+
+  public setProps = (nextProps: P) => {
+    if (!nextProps) {
+      return;
+    }
+
+    Object.assign(this.props, nextProps);
+  };
+
+  public get element() {
+    return this._element;
+  }
+
+  private _render() {
+    const block = this.render();
+    // Этот небезопасный метод для упрощения логики
+    // Используйте шаблонизатор из npm или напишите свой безопасный
+    // Нужно не в строку компилировать (или делать это правильно),
+    // либо сразу в DOM-элементы возвращать из compile DOM-ноду
+    this._element.innerHTML = block;
+  }
+
+  // Может переопределять пользователь, необязательно трогать
+  public render(): string {
+    return '';
+  }
+
+  getContent() {
+    return this.element;
+  }
+
+  private _makePropsProxy(props: P) {
+    // Можно и так передать this
+    // Такой способ больше не применяется с приходом ES6+
+    const self = this;
+
+    return new Proxy(props as unknown as object, {
+      get(target: Record<string, unknown>, prop: string) {
+        const value = target[prop];
+        return typeof value === 'function' ? value.bind(target) : value;
+      },
+      set(target: Record<string, unknown>, prop: string, value: unknown) {
+        target[prop] = value;
+
+        self.eventBus().emit(Block.EVENTS.FLOW_CDU, { ...target }, target);
+        return true;
+      },
+      deleteProperty() {
+        throw new Error('Нет доступа');
+      },
+    });
+  }
+
+  private _createDocumentElement(tagName: string) {
+    // Можно сделать метод, который через фрагменты в цикле создаёт сразу несколько блоков
+    return document.createElement(tagName);
+  }
+
+  public show() {
+    this.getContent().style.display = 'block';
+  }
+
+  public hide() {
+    this.getContent().style.display = 'none';
+  }
+}
---
+++ src/utils/event-bus.ts
@@ -0,0 +1,38 @@
+export type Listener<T extends unknown[] = any[]> = (...args: T) => void;
+
+export default class EventBus<E extends string = string, M extends {[K in E]: unknown[]} = Record<E, any[]>> {
+  private listeners: { [key in E]?: Listener<M[E]>[] } = {}
+
+
+  public on(event: E, callback: Listener<M[E]>) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event]!.push(callback);
+  }
+
+  public off(event: E, callback: Listener<M[E]>) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event]!.filter(
+      listener => listener !== callback
+    );
+  }
+
+  public emit(event: E, ...args: M[E]) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event]!.forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
+
+// const bus = new EventBus<'click'| 'input', {click: [MouseEvent]; input: [InputEvent]}>()
+//
+// bus.emit('click', )
