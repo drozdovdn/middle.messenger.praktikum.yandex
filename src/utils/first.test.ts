f119e1d9b1de12c862de9a6f8fff5e5b0a40e1eb
drozdov <drozdov@skytracking.ru>
2022-06-01 18:51:57 +0300

    add tes

--- src/utils/first.test.ts
+++
@@ -1,8 +0,0 @@
-import { assert } from 'chai';
-import { first } from './first';
-
-describe('Function first', () => {
-  it('Взятие первого элемента массива', () => {
-    assert.equal(first([1, 2, 3]), 1);
-  });
-});
