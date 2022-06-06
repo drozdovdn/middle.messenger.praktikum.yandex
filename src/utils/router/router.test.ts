8e31bb3ed8cc12821f7a148033ff23ab9c1832fa
drozdov <drozdov@skytracking.ru>
2022-06-06 12:47:25 +0300

    add test router

--- src/utils/router/router.test.ts
+++ src/utils/router/router.test.ts
@@ -1,22 +1,67 @@
 import Router from './router';
-import { assert } from 'chai';
+import { expect } from 'chai';
+import Block from '../block';
 
 describe('Router', () => {
-  describe('Router test', () => {
-    it('Router must be exists', function () {
+  it('should is singleton', () => {
+    const router = new Router('.root');
+    expect(new Router('.root')).to.eq(router);
+  });
+
+  describe('.use', () => {
+    it('should return Router instance', () => {
       const router = new Router('.root');
-      assert.exists(router);
+      const result = router.use('/', class {} as any);
+      expect(result).to.eq(router);
     });
-    it('routes must be empty', function () {
+  });
+
+  describe('.go', () => {
+    it('should render new block', () => {
+      class MyBlock extends Block {
+        constructor() {
+          super();
+        }
+        render(): DocumentFragment {
+          const fragment = document.createElement('template');
+          fragment.innerHTML = `<div id="test"></div>`;
+          return fragment.content;
+        }
+      }
       const router = new Router('.root');
-      assert.lengthOf(router.routes, 0);
+
+      router.use('/test', () => new MyBlock());
+      router.go('/test');
+      expect(document.getElementById('test')).not.to.be.null;
     });
-    // it("Router register pages", function () {
-    //   const router = new Router("#app");
-    //   router
-    //     .use("/sign-up", `<div>Sign-up page</div>`)
-    //     .use("/sign-in", `<div>Sign-in page</div>`);
-    //   assert.lengthOf(router.routes, 2);
-    // });
+  });
+
+  describe('Navigate test', () => {
+    it('Router initialized and router add', () => {
+      class MyBlock extends Block {
+        constructor() {
+          super();
+        }
+        render(): DocumentFragment {
+          const fragment = document.createElement('template');
+          fragment.innerHTML = `<div id="test"></div>`;
+          return fragment.content;
+        }
+      }
+      class Page extends MyBlock {}
+      class Post extends MyBlock {}
+      const router = new Router('.root');
+      router
+        .use('/block', () => new MyBlock())
+        .use('/page', () => new Page())
+        .use('/post', () => new Post())
+        .start();
+      router.go('/block');
+      router.go('/page');
+      router.go('/post');
+      // use '/'
+      // use '/test'
+      expect(router.routes.length).to.eq(5);
+    });
   });
 });
