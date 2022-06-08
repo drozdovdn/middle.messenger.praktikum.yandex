6e26ed784715738601987967e88b1bd703e092f4
drozdov <drozdov@skytracking.ru>
2022-06-08 15:34:17 +0300

    fix servet file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const PORT = 4000;
 
 const port = process.env.PORT || PORT;
@@ -7,6 +8,9 @@ const app = express();
 
 app.use(express.static('dist/legacy'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/legacy/index.html'));
+});
 app.listen(port, () => {
   console.log(`Сервер запущен на  http://localhost:${port}`);
 });
